441eecde70b89e0a87835c0d1df42e94eda56591
Ida Hoenigmann <i15013@student.htlwrn.ac.at>
2018-09-17 15:11:10 +0200

    1.5.3 method usage, struct FileInformation, method parse_argv
    usage prints help
    FileInformation stores filename and bool sorted
    parse_argv gets the commandline arguments and returns FileInformation

--- 01_report/src/Program.cs
+++ 01_report/src/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace report
+namespace i15013
 {
+    struct FileInformation
+    {
+        public string filename;
+        public bool sorted;
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            usage();
+            parse_argv(args);
+        }
+
+        static void usage()
+        {
+            Console.WriteLine("usage: report [--help|-h|-s] [FILE]");
+            Console.WriteLine("Print a sales statistics report ordered by product and salesclerk.\n");
+            Console.WriteLine("  --help|-h ... Help!");
+            Console.WriteLine("  -s ... sort it before producing the report");
+            Console.WriteLine("FILE ... file name or - (stdin). If FILE is missing read from stdin");
+            
+            Environment.Exit(1);
+        }
+
+        static FileInformation parse_argv(string[] argv)
+        {
+            FileInformation fileInformation = new FileInformation();
+            return fileInformation;
         }
     }
 }
