36b797bd533e0d1821e49e47796fb40924bb270f
Ida Hoenigmann <i15013@student.htlwrn.ac.at>
2018-09-17 15:45:52 +0200

    1.5.3 add parameter to usage; implement method parse_argv
    default parameter at method usage can be used to print an additional message
    parse_argv reads the commandline parameters and saves the information in an instance of FileInformation

--- 01_report/src/Program.cs
+++ 01_report/src/Program.cs
@@ -12,17 +12,21 @@ namespace i15013
     {
         static void Main(string[] args)
         {
-            usage();
             parse_argv(args);
         }
 
-        static void usage()
+        static void usage(string message = null)
         {
             Console.WriteLine("usage: report [--help|-h|-s] [FILE]");
             Console.WriteLine("Print a sales statistics report ordered by product and salesclerk.\n");
             Console.WriteLine("  --help|-h ... Help!");
             Console.WriteLine("  -s ... sort it before producing the report");
             Console.WriteLine("FILE ... file name or - (stdin). If FILE is missing read from stdin");
+
+            if (message != null)
+            {
+                Console.WriteLine("\n" + message);
+            }
             
             Environment.Exit(1);
         }
@@ -30,7 +34,46 @@ namespace i15013
         static FileInformation parse_argv(string[] argv)
         {
             FileInformation fileInformation = new FileInformation();
+
+            if (argv.Length >= 3)
+            {
+                usage("No more optioins or parameters allowed!\nAdditional argument was: '" + argv[2] + "'");
+            }
+
+            for (int i = 0; i < argv.Length; i++)
+            {
+                if (argv[i] == "-h" || argv[i] == "--help")
+                {
+                    usage();
+                }
+                else if (argv[i] == "-s")
+                {
+                    fileInformation.sorted = true;
+                }
+                else
+                {
+                    if (i == argv.Length - 1)
+                    {
+                        if (argv[i].StartsWith("-"))
+                        {
+                            usage("No more options allowed!\nAdditional option was: '" + argv[i] + "'");
+                        }
+                        fileInformation.filename = argv[i];
+                    }
+                    else
+                    {
+                        usage("Unknown option: " + argv[i]);
+                    }
+                }
+            }
+
+            if (fileInformation.filename == null)
+            {
+                fileInformation.filename = "-";
+            }
+            
             return fileInformation;
         }
+
     }
 }
