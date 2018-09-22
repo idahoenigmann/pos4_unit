654c2b9adcd651f9f6a9c9f0c1b0b4f2b2b1a332
Ida Hoenigmann <i15013@student.htlwrn.ac.at>
2018-09-22 11:45:48 +0200

    1.5.7 follow coding conventions

--- 01_report/src/Program.cs
+++ 01_report/src/Program.cs
@@ -1,139 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace i15013
-{
-    struct FileInformation
-    {
+namespace i15013 {
+    struct FileInformation {
         public string filename;
         public bool sorted;
     }
 
-    struct Record
-    {
+    struct Record {
         public string article;
         public string salesClerk;
         public double price;
         public int quantity;
         public int month;
 
-        public new string ToString()
-        {
-            return article + ", " + salesClerk + ", " + price + ", " + quantity + ", " + month;
+        public override string ToString() {
+            return article + ", " + salesClerk + ", " + price + ", "
+                   + quantity + ", " + month;
         }
     }
     
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
             process(parse_argv(args));
         }
 
-        static void usage(string message = null)
-        {
-            Console.WriteLine("usage: report [--help|-h|-s] [FILE]");
-            Console.WriteLine("Print a sales statistics report ordered by product and salesclerk.\n");
-            Console.WriteLine("  --help|-h ... Help!");
-            Console.WriteLine("  -s ... sort it before producing the report");
-            Console.WriteLine("FILE ... file name or - (stdin). If FILE is missing read from stdin");
+        static void usage(string message = null) {
+            Console.WriteLine("usage: report [--help|-h|-s] [FILE]\n" +
+            "Print a sales statistics report ordered by product and" +
+            "salesclerk.\n  --help|-h ... Help!\n" +
+            "  -s ... sort it before producing the report\n" +
+            "FILE ... file name or - (stdin). If FILE is missing read" +
+            "from stdin");
 
-            if (message != null)
-            {
+            if (message != null) {
                 Console.WriteLine("\n" + message);
             }
             
             Environment.Exit(1);
         }
 
-        static FileInformation parse_argv(string[] argv)
-        {
+        static FileInformation parse_argv(string[] argv) {
             FileInformation fileInformation = new FileInformation();
 
-            if (argv.Length >= 3)
-            {
-                usage("No more optioins or parameters allowed!\nAdditional argument was: '" + argv[2] + "'");
+            if (argv.Length < 1) {
+                usage();
+            }
+            
+            if (argv.Length >= 3) {
+                usage("No more optioins or parameters allowed!\nAdditional" +
+                      "argument was: '" + argv[2] + "'");
             }
 
-            for (int i = 0; i < argv.Length; i++)
-            {
-                if (argv[i] == "-h" || argv[i] == "--help")
-                {
+            for (int i = 0; i < argv.Length; i++) {
+                if (argv[i] == "-h" || argv[i] == "--help") {
                     usage();
-                }
-                else if (argv[i] == "-s")
-                {
+                } else if (argv[i] == "-s") {
                     fileInformation.sorted = true;
-                }
-                else
-                {
-                    if (i == argv.Length - 1)
-                    {
-                        if (argv[i].StartsWith("-"))
-                        {
-                            usage("No more options allowed!\nAdditional option was: '" + argv[i] + "'");
+                } else {
+                    if (i == argv.Length - 1) {
+                        if (argv[i].StartsWith("-")) {
+                            usage("No more options allowed!\nAdditional" +
+                                  "option was: '" + argv[i] + "'");
                         }
                         fileInformation.filename = argv[i];
-                    }
-                    else
-                    {
+                    } else {
                         usage("Unknown option: " + argv[i]);
                     }
                 }
             }
 
-            if (fileInformation.filename == null)
-            {
+            if (fileInformation.filename == null) {
                 fileInformation.filename = "-";
             }
             
             return fileInformation;
         }
 
-        static void process(FileInformation fileInformation)
-        {
+        static void process(FileInformation fileInformation) {
             List<Record> list = new List<Record>();
-            try
-            {
-                string[] fileContent = System.IO.File.ReadAllLines(fileInformation.filename);
+            try {
+                string[] fileContent =
+                    System.IO.File.ReadAllLines(fileInformation.filename);
                 int i = 0;
-                foreach (var line in fileContent)
-                {
+                foreach (var line in fileContent) {
                     i++;
                     String[] lineArray = line.Split(',');
                     
                     Record record = new Record();
                     record.article = lineArray[0];
                     record.salesClerk = lineArray[1];
-                    if (! double.TryParse(lineArray[2], out record.price))
-                    {
-                        Console.WriteLine(string.Format("Error in Line {0}. Price: {1}.", i, lineArray[2]));
+                    if (! double.TryParse(lineArray[2], out record.price)) {
+                        Console.WriteLine(string.Format("Error in Line {0}." +
+                                          "Price: {1}.", i, lineArray[2]));
                         Environment.Exit(3);
                     }
 
-                    if (!int.TryParse(lineArray[3], out record.quantity))
-                    {
-                        Console.WriteLine(string.Format("Error in Line {0}. Quantity: {1}.", i, lineArray[3]));
+                    if (!int.TryParse(lineArray[3], out record.quantity)) {
+                        Console.WriteLine(string.Format("Error in Line {0}." +
+                                          "Quantity: {1}.", i, lineArray[3]));
                         Environment.Exit(3);
                     }
 
-                    if (!int.TryParse(lineArray[4], out record.month))
-                    {
-                        Console.WriteLine(string.Format("Error in Line {0}. Month: {1}.", i, lineArray[4]));
+                    if (!int.TryParse(lineArray[4], out record.month)) {
+                        Console.WriteLine(string.Format("Error in Line {0}." +
+                                          "Month: {1}.", i, lineArray[4]));
                         Environment.Exit(3);
                     }
 
                     list.Add(record);
                 }
-            }
-            catch (System.IO.FileNotFoundException)
-            {
-                Console.WriteLine(string.Format("File '{0}' not found!", fileInformation.filename));
+            } catch (System.IO.FileNotFoundException) {
+                Console.WriteLine(string.Format("File '{0}' not found!",
+                    fileInformation.filename));
                 Environment.Exit(2);
             }
 
-            foreach (var record in list)
-            {
+            foreach (var record in list) {
                 Console.WriteLine(record.ToString());
             }
         }
