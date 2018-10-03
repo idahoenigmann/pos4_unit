fdf10dc591bd2cd68bb14a0c42a005832acc332e
Ida Hoenigmann <i15013@student.htlwrn.ac.at>
2018-10-03 18:38:14 +0200

    2.4.1.1 write finite automaton reading commandline arguments

--- 02_elispy/src/Elispy.cs
+++ 02_elispy/src/Elispy.cs
@@ -2,13 +2,20 @@
 
 namespace i15013
 {
+    struct FileInformation {
+        public string filename;
+        public bool g;
+    }
+    
     class Program
     {
         static void Main(string[] args) {
-            usage();
+            FileInformation fileInformation = parse_argv(args);
+            Console.WriteLine(fileInformation.filename);
+            Console.WriteLine(fileInformation.g);
         }
         
-        static void usage(string message = null) {
+        public static void usage(string message = null) {
             Console.WriteLine("usage: elispy [--help|-h|-s] [FILE]\n" +
                               "Executes the \"elispy\" expressions contained " +
                               "in FILE otherwise the REPL will be started\n\n" +
@@ -24,5 +31,34 @@ namespace i15013
             Environment.Exit(1);
         }
 
+        private static FileInformation parse_argv(string[] args) {
+            FileInformation fileInformation = new FileInformation();
+            fileInformation.filename = "-";
+            fileInformation.g = false;
+            switch (args.Length) {
+                case 0:
+                    break;
+                case 1 when args[0] == "--help" | args[0] == "-h":
+                    usage();
+                    break;
+                case 1 when args[0] == "-g":
+                    fileInformation.g = true;
+                    break;
+                case 1:
+                    fileInformation.filename = args[0];
+                    break;
+                case 2 when args[0] != "-g":
+                    usage($"illegal argument {args[0]}");
+                    break;
+                case 2:
+                    fileInformation.g = true;
+                    fileInformation.filename = args[1];
+                    break;
+                default:
+                    usage();
+                    break;
+            }
+            return fileInformation;
+        }
     }
 }
