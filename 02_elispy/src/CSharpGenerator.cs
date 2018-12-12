9925039f2fd519bc0979f19f67c438bcde8a8bd0
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2018-12-12 15:45:50 +0100

    add methods visit to CSharpGenerator

--- 02_elispy/src/CSharpGenerator.cs
+++ 02_elispy/src/CSharpGenerator.cs
@@ -1,8 +1,10 @@
 using i15013.elispy;
 
-
 namespace i15013.transpiler {
-    public class CSharpGenerator {
-        
+    public class CSharpGenerator : CodeGenerator {
+        public void visit(SexpSymbol sexpsym) {}
+        public void visit(SexpString sexpstr) {}
+        public void visit(SexpInteger sexpint) {}
+        public void visit(SexpList sexplst) {}
     }
 }
