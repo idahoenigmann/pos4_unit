043919cd8c47c6994d8dbf5557a4b755c369ccb1
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2019-01-16 14:36:36 +0100

    add operators !, -> and <->

--- 03_logic_sim/logics_printer/src/Program.cs
+++ 03_logic_sim/logics_printer/src/Program.cs
@@ -3,7 +3,7 @@ using i15013.logics.propositional;
 
 namespace i15013.logics_printer
 {
-    static class Printer {
+    static public class Printer {
         static public void print_table(Func<bool, bool, bool> op) {
             Console.WriteLine($" 0 | 0 |   {System.Convert.ToInt32(op(false, false))}");
             Console.WriteLine($" 0 | 1 |   {System.Convert.ToInt32(op(false, true))}");
@@ -12,16 +12,18 @@ namespace i15013.logics_printer
         }
         
         static public void print_table(Func<bool, bool> op) {
-            Console.WriteLine($" 0 | 0 |   {System.Convert.ToInt32(op(false, false))}");
-            Console.WriteLine($" 0 | 1 |   {System.Convert.ToInt32(op(false, true))}");
-            Console.WriteLine($" 1 | 0 |   {System.Convert.ToInt32(op(true, false))}");
-            Console.WriteLine($" 1 | 1 |   {System.Convert.ToInt32(op(true, true))}");
+            Console.WriteLine($" 0 |   {System.Convert.ToInt32(op(false))}");
+            Console.WriteLine($" 1 |   {System.Convert.ToInt32(op(true))}");
         }
         
-        static public void print_head(string sym) {
-            Console.WriteLine($" a | b | a {sym} b");
-            Console.WriteLine("---------------");
-
+        static public void print_head(string sym, int args) {
+            if (args == 2) {
+                Console.WriteLine($" a | b | a {sym} b");
+                Console.WriteLine("---------------");
+            } else if (args == 1) {
+                Console.WriteLine($" a |  {sym}a");
+                Console.WriteLine("-------------");
+            }
         }
     }
     
@@ -32,16 +34,29 @@ namespace i15013.logics_printer
             Console.WriteLine("Propositional Logic");
             Console.WriteLine("===================\n");
 
-            Printer.print_head("&");
+            Printer.print_head("&", 2);
             Printer.print_table(Operators.conj);
             Console.WriteLine("");
             
-            Printer.print_head("|");
+            Printer.print_head("|", 2);
             Printer.print_table(Operators.disj);
             Console.WriteLine("");
             
-            Printer.print_head("^");
+            Printer.print_head("^", 2);
             Printer.print_table(Operators.anti);
+            Console.WriteLine("");
+            
+            Printer.print_head("!", 1);
+            Printer.print_table(Operators.neg);
+            Console.WriteLine("");
+
+            Printer.print_head("->", 2);
+            Printer.print_table(Operators.imp);
+            Console.WriteLine("");
+
+            
+            Printer.print_head("<->", 2);
+            Printer.print_table(Operators.equiv);
             Console.WriteLine("");
 
         }
