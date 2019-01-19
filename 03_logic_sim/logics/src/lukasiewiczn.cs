11fc13686a7a8f0c59bf7be6eede68f92a491af5
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2019-01-19 16:45:37 +0100

    add Ln logic

---
+++ 03_logic_sim/logics/src/lukasiewiczn.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace i15013.logics.lukasiewiczn {
+    public class Operators {
+        public static double conj(double op1, double op2) {
+            return Math.Min(op1, op2);
+        }
+        
+        public static double disj(double op1, double op2) {
+            return Math.Max(op1, op2);
+        }
+        
+        public static double neg(double op1) {
+            return 1 - op1;
+        }
+        
+        public static double anti(double op1, double op2) {
+            return conj(disj(op1, op2), neg(conj(op1, op2)));
+        }
+        
+        public static double imp(double op1, double op2) {
+            return Math.Min(1, 1 + op2 - op1);
+        }
+        
+        public static double equiv(double op1, double op2) {
+            return 1 - Math.Abs(op1 - op2);
+        }
+    }
+}
